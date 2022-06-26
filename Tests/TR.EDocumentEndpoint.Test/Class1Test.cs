9d4bc5378759a0207e954e51e896eb46e2bf6e1b
Ahmet AK <ahmeetaak@gmail.com>
2022-06-26 14:35:25 +0300

    Update Class1Test.cs

--- Tests/TR.EDocumentEndpoint.Test/Class1Test.cs
+++ Tests/TR.EDocumentEndpoint.Test/Class1Test.cs
@@ -8,8 +8,6 @@ public class Class1Test
     [Fact]
     public void Given_When_Then()
     {
-        var class1 = new Class1();
-
-        Assert.NotNull(class1);
+        Assert.True(true);
     }
 }
